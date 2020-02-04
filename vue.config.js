031cc4efc4e5030dd0304f043f147c9e42085fb2
JS <parkjjss2@gmail.com>
2020-02-04 15:40:39 +0900

    ..

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+//웹팩 설정을 위해 vue.config.js라는 파일을 프로젝트 내부에 작성
+// https://cli.vuejs.org/config/#vue-config-js
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+   
+  publicPath : "./",               //index.html 의 css, js link 시작점
+  outputDir: "../webapp./",    // css, js, index.html 배포경로
+  indexPath: "../../WEB-INF/index.jsp",  // index.html 위치, 이름
+  devServer: {  
+    //proxy: "http://localhost:8080"  
+  },  
+
+ 
 }
